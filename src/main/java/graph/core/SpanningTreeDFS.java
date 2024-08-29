760edb7746b38ec81432220215e2d79c14e045fd
Renato Serrano <renatom.sandrade@gmail.com>
2024-08-28 23:49:35 -0300

    feat: create spanning tree generator from DFS algorithm

--- src/main/java/graph/core/SpanningTreeDFS.java
+++ src/main/java/graph/core/SpanningTreeDFS.java
@@ -10,4 +10,30 @@ public class SpanningTreeDFS {
     this.spanningTree = new Graph();
     this.visited = new HashSet<>();
   }
+
+  public Graph createSpanningTree() throws Exception {
+    if (originalGraph.getVertices().isEmpty()) {
+      return spanningTree;
+    }
+
+    String startVertex = originalGraph.getVertices().get(0).getLabel();
+    dfs(startVertex);
+
+    return spanningTree;
+  }
+
+  private void dfs(String currentVertexLabel) throws Exception {
+    visited.add(currentVertexLabel);
+    spanningTree.AddVertex(currentVertexLabel);
+
+    List<Vertex> adjacencies = originalGraph.getAdjacenciess(currentVertexLabel);
+    for (Vertex adjacentVertex : adjacencies) {
+      String adjacentLabel = adjacentVertex.getLabel();
+      if (!visited.contains(adjacentLabel)) {
+        spanningTree.AddVertex(adjacentLabel);
+        spanningTree.connectVertices(currentVertexLabel, adjacentLabel);
+        dfs(adjacentLabel);
+      }
+    }
+  }
 }
